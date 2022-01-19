94964cbc726695d9ad67c028d6cdd859c1760e10
Xing <x.xing.unsw@gmail.com>
2022-01-19 20:56:12 +1100

    implement right tab

--- src/components/rights/rights.js
+++ src/components/rights/rights.js
@@ -1,5 +1,21 @@
 export default {
   data () {
-    return {}
+    return {
+      rightData: []
+    }
+  },
+  created () {
+    this.loadRightData()
+  },
+  methods: {
+    async loadRightData () {
+      let res = await this.$axios.get(`rights/list`)
+      if (res.data.meta.status === 200) {
+        this.rightData = res.data.data
+      }
+    },
+    indexMethod (index) {
+      return index
+    }
   }
 }
